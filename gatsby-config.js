968e8d58256b0fce9b6df28a2f2a7a01dd7d3c22
Kevin Ang <ka@kevinang.com>
2021-09-19 15:22:56 -0500

    fix: disable partial updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -151,7 +151,7 @@ module.exports = {
         //   // optional, any index settings
         //   // Note: by supplying settings, you will overwrite all existing settings on the index
         // },
-        enablePartialUpdates: true, // default: false
+        // enablePartialUpdates: true, // default: false
         // matchFields: ['slug', 'modified'], // Array<String> default: ['modified']
         // concurrentQueries: false, // default: true
         // skipIndexing: true, // default: false, useful for e.g. preview deploys or local development
