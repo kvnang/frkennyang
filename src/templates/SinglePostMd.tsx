85c7b2881361cabf8ea1b714eb1184a3f2d00f22
Kevin Ang <ka@kevinang.com>
2021-09-19 15:05:22 -0500

    builds

--- src/templates/SinglePostMd.tsx
+++ src/templates/SinglePostMd.tsx
@@ -135,10 +135,6 @@ const SinglePostStyles = styled.div`
       left: 0;
     }
   }
-
-  a {
-    ${inlineLink()};
-  }
 `;
 
 const PostContentStyles = styled.div`
@@ -149,6 +145,9 @@ const PostContentStyles = styled.div`
 
   ul,
   ol {
+    &:not(:last-child) {
+      margin-bottom: 1.5rem;
+    }
     li {
       &:not(:last-child) {
         margin-bottom: 1.5rem;
@@ -160,6 +159,10 @@ const PostContentStyles = styled.div`
     margin: 1.5rem 0;
     border-color: var(--grey);
     border-width: 1px;
+  }
+
+  a {
+    ${inlineLink()};
   }
 
   // Gatsby Image
@@ -179,6 +182,25 @@ const PostContentStyles = styled.div`
     }
   }
 
+  .gatsby-img-attributes {
+    &[style*='float'] {
+      width: 100%;
+      max-width: 45%;
+      margin: 0.25rem 1rem 1rem 0;
+
+      @media ${breakpoints.mobileOnly} {
+        float: none !important;
+        margin: 0 !important;
+        max-width: 100%;
+      }
+    }
+
+    &[style*='float: right'] {
+      margin-left: 1rem;
+      margin-right: 0;
+    }
+  }
+
   // Footnote
 
   a.footnote-ref,
@@ -186,6 +208,13 @@ const PostContentStyles = styled.div`
     text-decoration: none;
     color: var(--gold);
     font-weight: bold;
+  }
+
+  // Title link
+  a.anchor svg {
+    fill: var(--color-accent);
+    height: 1rem;
+    width: auto;
   }
 `;
 
