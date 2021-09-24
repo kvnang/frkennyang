23b5e4a5e7d0e993937bdc55786190a0ba224906
Kevin Ang <ka@kevinang.com>
2021-09-24 08:48:23 -0500

    cleanup

--- src/api/submit.js
+++ src/api/submit.js
@@ -15,15 +15,15 @@ const mg = mailgun({
 
 export default async function handler(req, res) {
   if (!req.body) {
-    res.status(400).json({});
+    res.status(400).send(`Form data is required.`);
+    return;
   }
 
-  const { 'form-name': formName, ...allData } = req.body;
-
-  const { title, ...data } = allData; // title is honeypot on both forms
+  const { 'form-name': formName, title, ...data } = req.body; // title is honeypot on both forms
 
   if (!formName || data.title) {
-    res.status(400).json(`Sorry ... beep bop ... this email cannot be sent.`);
+    res.status(400).send(`Sorry ... beep bop ... this email cannot be sent.`);
+    return;
   }
 
   // Log to Google Sheet
@@ -139,7 +139,7 @@ export default async function handler(req, res) {
   mg.messages().send(mailOptions, (error, body) => {
     if (error) {
       console.log(error);
-      res.status(400).json(error);
+      res.status(500).json(error);
     } else {
       console.log(body);
       res.status(200).json(body);
