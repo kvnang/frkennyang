d8b91099bf00b975c8675ba4b488943641862035
Kevin Ang <51168758+kvnang@users.noreply.github.com>
2022-03-26 07:08:14 -0600

    Debug

--- src/api/submit.ts
+++ src/api/submit.ts
@@ -228,7 +228,11 @@ export default async function handler(
   </mjml>
   `;
 
-  console.log('Before MJML', req.body);
+  console.log('Before MJML', mjml);
+
+  if (!process.env.MJML_APPLICATION_ID || !process.env.MJML_SECRET_KEY) {
+    console.log('MJML_APPLICATION_ID or MJML_SECRET_KEY not set');
+  }
 
   const mjmlResponse: Response = await fetch('https://api.mjml.io/v1/render', {
     method: 'POST',
