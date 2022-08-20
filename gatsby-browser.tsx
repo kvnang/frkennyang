19d26c149cf798724d8a60057fa9bdae41f5296c
Kevin Ang <51168758+kvnang@users.noreply.github.com>
2022-08-19 19:58:50 -0500

    fix: improved browser-side lang definition

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -4,8 +4,25 @@ import Layout from './src/components/Layout';
 import '@fontsource/playfair-display/700.css';
 import '@fontsource/montserrat/400.css';
 import '@fontsource/montserrat/600.css';
+import { slugify } from './src/utils/helpers';
 
 export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({
   element,
   props,
 }) => <Layout {...props}>{element}</Layout>;
+
+export const onRouteUpdate: GatsbyBrowser['onRouteUpdate'] = ({
+  location: { pathname },
+}) => {
+  // set html attribute
+  document.documentElement.setAttribute(
+    'lang',
+    pathname.startsWith('/id/') ? 'id' : 'en'
+  );
+
+  // set body attribute
+  const pageSlug = slugify(pathname) || 'home';
+  document.body.className = `page-${pageSlug} ${
+    pageSlug === 'contents' ? 'footer-light' : ''
+  }`;
+};
