e329a1ebe0cd9170a110fcc5df9d68898578ed2e
Kevin Ang <51168758+kvnang@users.noreply.github.com>
2023-05-05 21:50:02 -0600

    fix: border

--- app/[lang]/blog/(archive)/category/[category]/page.tsx
+++ app/[lang]/blog/(archive)/category/[category]/page.tsx
@@ -1,29 +1,14 @@
 import { type LangType } from "@/types";
-import BlogPage from "../../page";
-import { getMetadata } from "@/lib/metadata";
-import client from "@/lib/sanity.client";
+import BlogPage, {
+  generateMetadata as blogPageGenerateMetadata,
+} from "../../page";
 
 export async function generateMetadata({
   params,
 }: {
   params: { lang: LangType; category?: string };
 }) {
-  const category = params.category
-    ? await client.fetch(
-        `*[_type == "category" && slug.current == $slug] {
-      title
-    }[0]`,
-        {
-          slug: params.category,
-        }
-      )
-    : null;
-
-  return getMetadata({
-    title: category ? `${category.title} | Blog` : "Blog",
-    pathname: `/${params.lang}/blog`,
-    description: `Browse Fr. Kenny's latest works, available in both English and Bahasa Indonesia.`,
-  });
+  return blogPageGenerateMetadata({ params });
 }
 export default async function BlogCategoryPage({
   params,
--- app/[lang]/blog/(archive)/page.tsx
+++ app/[lang]/blog/(archive)/page.tsx
@@ -61,7 +61,7 @@ export default async function BlogPage({
 
   return (
     <>
-      <div className="pb-10 mb-10 border-b border-gray">
+      <div className="pb-10 mb-10 border-b border-medium-gray last:border-none last:mb-0 last:pb-0">
         <PostEntry
           post={featuredPost}
           showImage
@@ -69,13 +69,15 @@ export default async function BlogPage({
           showExcerpt
         />
       </div>
-      <div>
-        <BlogList
-          params={params}
-          searchParams={searchParams}
-          initialData={recentPosts}
-        />
-      </div>
+      {recentPosts.length > 0 && (
+        <div>
+          <BlogList
+            params={params}
+            searchParams={searchParams}
+            initialData={recentPosts}
+          />
+        </div>
+      )}
     </>
   );
 }
